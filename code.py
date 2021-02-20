de138afcc5f118ddb7bb0da282249c365423bea5
Matthew Echert <mgechert@gmail.com>
2021-02-19 18:16:18 -0800

    fixed displayio import

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 import time
 import board
-from displayio import TileGrid, Group
+from displayio import TileGrid, Group, release_displays
 import gc
 from scrambler import Scrambler
 from adafruit_matrixportal.matrixportal import MatrixPortal
@@ -20,14 +20,6 @@ mp = MatrixPortal(
 def log_free_mem(txt="Memory free"):
     print(f"mem_free() {txt} {gc.mem_free()} bytes")
 
-# def load_scrambled_bmp(file):
-#     return adafruit_imageload.load(
-#         file,
-#         bitmap=Scrambler,
-#         palette=displayio.Palette,
-#     )
-
-
 log_free_mem('after init')
 scrambler, palette = Scrambler.load_bitmap('./images/rainbow2.bmp')
 log_free_mem('Img loaded')
@@ -40,7 +32,7 @@ mp.display.show(group)
 
 # Turn it off after 15s so it doesn't glare in my eyes...
 time.sleep(15.0)
-displayio.release_displays()
+release_displays()
 
 while True:
     pass

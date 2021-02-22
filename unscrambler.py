877dc41f4b7eccd7125f5625edf640339fdfc7aa
Matthew Echert <mgechert@gmail.com>
2021-02-22 13:05:15 -0800

    Added bubblesort, refactor w\unscramble_row()

--- unscrambler.py
+++ unscrambler.py
@@ -4,11 +4,13 @@ class Unscrambler:
     def __init__(self, scrambler):
         self.scrambler = scrambler
 
-    def quicksort(self, row):
-        arr = self.scrambler.matrix[row]
+    def unscramble_row(self, row, sorter):
         def swap(i, j):
             self.scrambler.swap_cols(row, i, j)
-
+        sorter(self.scrambler.matrix[row], swap)
+        
+    @classmethod
+    def quicksort(cls, arr, swap):
         def qs(start, end):
             if start >= end:
                 return
@@ -26,3 +28,10 @@ class Unscrambler:
             qs(i+1, end)
         
         qs(0, len(arr))
+
+    @classmethod
+    def bubblesort(cls, arr, swap):
+        for j in range(len(arr)):
+            for i in range(len(arr)-j-1):
+                if arr[i] > arr[i+1]:
+                    swap(i, i+1)
